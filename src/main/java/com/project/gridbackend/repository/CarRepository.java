48f671b1fa543098a37c022a541f68e3824138e9
Thodoris Goulas <t.goulas@paymentcomponents.com>
2021-11-26 02:11:35 +0200

    Initial commit

--- src/main/java/com/project/gridbackend/repository/CarRepository.java
+++ src/main/java/com/project/gridbackend/repository/CarRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 public interface CarRepository extends PagingAndSortingRepository<Car, Long> {
-	@Query("SELECT c.* FROM Cars c WHERE (:make is null or c.make = :make) and (:model is null"
-			+ " or c.model = :model) and (:priceFrom is null or priceFrom <= c.price) and (:priceTo is null or priceTo >= c.price)")
+	@Query("SELECT c FROM Cars c WHERE (:make is null or c.make = :make) and (:model is null"
+			+ " or c.model = :model) and (:priceFrom is null or :priceFrom <= c.price) and (:priceTo is null or :priceTo >= c.price)")
 	Page<Car> findAllByMakeAndModelAndPriceBetween(String make, String model, Long priceFrom, Long priceTo, Pageable pageable);
 }
